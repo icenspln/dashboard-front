6e74a5b8e57d337c4e41487ac46841db05deff70
OmarBknd <omar.bknd@gmail.com>
2024-07-17 23:42:59 +0100

    added the link to the register form button

---
+++ src/routes/group-management/GroupManagement.tsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import ButtonPrimary from "../../components/ButtonPrimary";
+import {GroupsTable,} from "./groups-table/GroupsTable"
+
+
+
+export default function GroupManagement() {
+  
+;
+
+  
+  
+  return (
+    <section className="w-full min-h-screen p-4 bg-mainBg">
+       <nav className="flex items-center justify-end mb-4">
+        <Link to={`/groupmanagement/new`}>
+          <ButtonPrimary text="إضافة فوج جديد" active />
+        </Link>
+        
+        
+      </nav>
+      <div>
+        <GroupsTable  />
+      </div>
+    </section>
+  );
+}
