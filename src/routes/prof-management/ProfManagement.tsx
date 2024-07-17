cefb002f5849c6a8b96b54e959d2e9849ab13d73
OmarBknd <omar.bknd@gmail.com>
2024-07-17 23:45:10 +0100

    added a link to the register button

--- src/routes/prof-management/ProfManagement.tsx
+++ src/routes/prof-management/ProfManagement.tsx
@@ -1,7 +1,27 @@
+import { Link } from "react-router-dom";
+import ButtonPrimary from "../../components/ButtonPrimary";
+import {ProfsTable,} from "./profs-table/ProfsTable"
+
+
+
 export default function ProfManagement() {
+  
+;
+
+  
+  
   return (
-    <section>
-      <nav>prof</nav>
+    <section className="w-full min-h-screen p-4 bg-mainBg">
+       <nav className="flex items-center justify-end mb-4">
+        <Link to={`/profmanagement/new`}>
+          <ButtonPrimary text="تسجيل جديد" active />
+        </Link>
+        
+        
+      </nav>
+      <div>
+        <ProfsTable  />
+      </div>
     </section>
   );
 }
