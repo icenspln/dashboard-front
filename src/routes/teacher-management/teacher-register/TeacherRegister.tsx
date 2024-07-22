51dc006c44708e941fcc817db3274a499eb73a8f
icenspln <icenboat@gmail.com>
2024-07-22 18:00:19 +0100

    removed recursive dom

--- src/routes/teacher-management/teacher-register/TeacherRegister.tsx
+++ src/routes/teacher-management/teacher-register/TeacherRegister.tsx
@@ -6,7 +6,6 @@ export default function TeacherRegister() {
       </div>
       <div>
         {/* form */}
-        <TeacherRegister />
         {/* <article className="flex flex-col gap-1 items-center h-[435px] w-[391px] absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 bg-white px-[20px] py-[27px] rounded-3xl">
             <h2 className="text-lg text-center mb-3 font-bold text-blueDark">
               تسجيل البطاقة الذكية
