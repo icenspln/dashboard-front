2cd97088f666d1c9b6387a11e0ad3821a7afa4f6
OmarBknd <omar.bknd@gmail.com>
2024-07-24 11:22:49 +0100

    added the confirm button

--- src/routes/group-management/groups-table/core/columns/overlays/addNewSession.tsx
+++ src/routes/group-management/groups-table/core/columns/overlays/addNewSession.tsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import { Overlay } from '../../../../../../components/Overlay';
+import ConfirmButton from './confirmButton';
 
 interface AddNewSessionOverlayProps {
   onClose: () => void;
@@ -10,8 +11,19 @@ const AddNewSessionOverlay: React.FC<AddNewSessionOverlayProps> = ({ onClose }) 
   return (
     <Overlay onClose={onClose}>
       <>
+
+      <div className=' flex flex-col  items-center w-[339] h-[192px] gap-[15px] '>
         <h1 className="text-2xl">إضافة حصة للفوج</h1>
-        <p></p>
+        <p>يرجى اختيار اليوم و الوقت لإضافة الحصة</p>
+
+        <span className='flex justify-between'>
+        <input type="date"  />
+        <input type="time" />
+        </span>
+        <span className='flex justify-center'>
+          <ConfirmButton text="تسجيل التغييرات" color='bg-blue'/>
+        </span>
+        </div>
       </>
     </Overlay>
   );
