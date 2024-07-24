db8ba572040c3bb7fc0a5147ac5b31a3fcde3b9e
OmarBknd <omar.bknd@gmail.com>
2024-07-24 18:18:19 +0100

    added search bar field to the presence managment table

--- src/routes/presence-management/PresenceManagement.tsx
+++ src/routes/presence-management/PresenceManagement.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
-
+import ExcelSvg from "../../assets/icons/ExcelSvg";
 import { PresenceListsTable } from "./presence-table/PresenceTable";
+import SearchBar from "../../components/SearchBar";
 
 
 
@@ -12,9 +13,10 @@ export default function PresenceListsManagement() {
   
   return (
     <section className="w-full min-h-screen p-4 bg-mainBg">
-       <nav className="flex items-center justify-end mb-4">
+       <nav className="flex items-center justify-between mb-4">
+       <SearchBar/>
         <Link to={`/presencemanagement/`}>
-         
+         <ExcelSvg/>
         </Link>
         
         
