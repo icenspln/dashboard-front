21343ea8c818b8d3ffafcad8d27fc570e6a52bc0
OmarBknd <omar.bknd@gmail.com>
2024-07-25 13:55:43 +0100

    fix the seach bar width

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,9 +1,9 @@
 
 export default function SearchBar() {
   return (
-    <div className="flex items-center rounded border-2 border-gray  min-w-[241px] min-h-[40px]">
+    <div className="flex items-center rounded border-2 border-gray  min-w-[241px] h-[32px]">
       <input
-        className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+        className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 px-3 py-2  leading-tight focus:outline-none"
         type="text"
         placeholder="البحث في القائمة"
       />
