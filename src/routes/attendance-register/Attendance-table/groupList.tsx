6f0f65e51b273bc2f99b6cdb16a7729cdd65f7d9
OmarBknd <omar.bknd@gmail.com>
2024-07-27 13:18:42 +0100

    added a div for the rest of the week groups

--- src/routes/attendance-register/Attendance-table/groupList.tsx
+++ src/routes/attendance-register/Attendance-table/groupList.tsx
@@ -3,10 +3,23 @@ import PresentButton from "./core/columns/overlays/Popup-menu-component/isPresen
 
 export default function GroupList(){
     return(
-        <div className="w-[637px] h-[303px] mt-10  border border-gray-200 rounded-lg shadow-sm">
+        <div className="flex flex-col gap-[12px] w-[637px] h-[303px] mt-10  border border-gray-200 rounded-lg shadow-sm">
             <div>
-                <p className="text-gray-500 px-3 py-3">قائمة الأفواج لهذا اليوم</p>
-                <div className=" flex justify-between px-4">
+                <p className="text-sm text-gray-500 px-3 py-3">قائمة الأفواج لهذا اليوم</p>
+                <div className=" flex justify-between px-5">
+                    <div className="flex gap-[10px] items-center ">
+                        <p>الرياضيات | علي رياد | الأحد - 03:00</p>
+                        <span className="w-[68px] h-[20px] text-sm text-center border rounded-full">2000 دج</span>
+                    </div>
+                    <div className="flex gap-[8px]">
+                        <PresentButton/>
+                        <SettingsCell/>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <p className="text-sm text-gray-500 px-3 py-3">قائمة الأفواج الأخرى</p>
+                <div className=" flex justify-between px-5">
                     <div className="flex gap-[10px] items-center ">
                         <p>الرياضيات | علي رياد | الأحد - 03:00</p>
                         <span className="w-[68px] h-[20px] text-sm text-center border rounded-full">2000 دج</span>
