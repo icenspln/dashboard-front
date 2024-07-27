91e524c2330b099717e0c4ff33e9eb8dfbac7bce
OmarBknd <omar.bknd@gmail.com>
2024-07-27 14:11:49 +0100

    change the table style

--- src/routes/attendance-register/Attendance-table/studentPaymentTable.tsx
+++ src/routes/attendance-register/Attendance-table/studentPaymentTable.tsx
@@ -5,10 +5,7 @@ import {
   } from "@tanstack/react-table";
   import { studentPaymentColumns } from "./core/columns/columns";
   import { Student } from "./core/_models";
-
-
- 
- 
+  import ConfirmButton from "./core/columns/overlays/Popup-menu-component/confirmButton";
   import  data from "../Attendance-table/core/data.json" 
   
   export function StudentPaymentTable() {
@@ -25,49 +22,43 @@ import {
     });
   
     return (
-      <div className="w-[637px] mt-10  border border-gray-200 rounded-lg shadow-sm">
-      <table className="w-full rounded-lg bg-white ">
-        <thead className="">
-          {table.getHeaderGroups().map((headerGroup) => {
-            return (
-              <tr className="h-[61px]  rounded-lg px-4 py-2 border-b text-center text-blue" key={headerGroup.id}>
-                {headerGroup.headers.map(
-                  (
-                    header // map over the headerGroup headers array
-                  ) => (
-                    <th
-                      key={header.id}
-                      className="text-textGray text-start p-3 font-normal"
-                      colSpan={header.colSpan}
-                    >
-                      {flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                      )}
-                    </th>
-                  )
-                )}
-              </tr>
-            );
-          })}
-        </thead>
-        <tbody>
-          {table.getRowModel().rows.map((row) => (
-            <tr className="border border-b-light" key={row.id}>
-              {row.getAllCells().map((cell) => {
-                return (
-                  <td
-                    className="text-darkGray text-start p-3 font-normal"
-                    key={cell.id}
-                  >
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </td>
-                );
-              })}
-            </tr>
+      <div className="max-w-4xl w-[637px] mt-10  border border-gray-200 rounded-lg shadow-sm">
+      <div className="border rounded-lg shadow-md overflow-hidden">
+        <div className="bg-gray-100">
+          {table.getHeaderGroups().map((headerGroup) => (
+            <div key={headerGroup.id} className="grid grid-cols-3 divide-x divide-gray-300">
+              {headerGroup.headers.map((header) => (
+                <div key={header.id} className="text-center py-2">
+                  {flexRender(
+                    header.column.columnDef.header,
+                    header.getContext()
+                  )}
+                </div>
+              ))}
+            </div>
           ))}
-        </tbody>
-      </table>
+          {table.getRowModel().rows.map((row) => (
+            <div key={row.id} className="grid grid-cols-3 divide-x divide-gray-300 py-2">
+              {row.getVisibleCells().map((cell) => (
+                <div key={cell.id} className="text-center  flex justify-center align-center">
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+        <div className="flex items-center justify-between p-4 bg-white gap-[12px]">
+         
+          <input
+            type="text"
+            className="w-full border border-gray-300 outline-gray-300 rounded-lg p-2 w-1/2"
+    
+            placeholder="2000 دج"
+            
+          />
+          <ConfirmButton text="تسجيل دفع جديد" color="bg-blue" textColor="text-white"/>
+        </div>
+      </div>
     </div>
     );
   }
