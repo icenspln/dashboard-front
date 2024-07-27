74d9857a8755eed4b3bf3a616cb898399d4667fb
OmarBknd <omar.bknd@gmail.com>
2024-07-27 02:17:32 +0100

    added the link to the registeration display

---
+++ src/routes/attendance-register/AttendanceManagement.tsx
@@ -0,0 +1,14 @@
+
+import { NavigateToRegister } from "./Attendance-navigation-page/NavigateToRegister";
+
+
+
+export default function AttendanceManagement() {
+  return (
+    <section className="w-full min-h-screen bg-mainBg shadow-xl">
+     
+     
+      <NavigateToRegister/>
+    </section>
+  );
+}
