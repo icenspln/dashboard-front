abf2f1a9b0fff78aa69d7759057c9f1322d72a7f
OmarBknd <omar.bknd@gmail.com>
2024-07-27 13:17:37 +0100

    added cardless registration link

--- src/routes/attendance-register/Attendance-table/tablesContainer.tsx
+++ src/routes/attendance-register/Attendance-table/tablesContainer.tsx
@@ -2,17 +2,22 @@ import CardAnimationSvg from "../../../assets/icons/CardAnimationSvg";
 import { StudentInfoTable } from "./studentInfoTable";
 import { StudentPaymentTable } from "./studentPaymentTable";
 import GroupList from "./groupList";
+import CardlessRegister from "../Attendance-navigation-page/overlays/cardlessRegisterLink";
 
 export default function TablesContainer(){
   return(
     
-    <div className="flex w-full min-h-screen p-4 bg-mainBg">
+    <div className="flex w-full gap-[25px] min-h-screen p-4 bg-mainBg">
 
     <div>
       <StudentInfoTable />
       <StudentPaymentTable />
       <GroupList/>
+      <div className="flex justify-end text-xl py-1">
+        <CardlessRegister/>
+        </div>
     </div>
+    
     <div className="flex justify-center items-center aligns-center">
       <CardAnimationSvg />
     </div>
