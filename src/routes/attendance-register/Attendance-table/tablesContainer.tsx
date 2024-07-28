38ae2cb87b1adc70708022e2cfbdbbd45999c8d2
OmarBknd <omar.bknd@gmail.com>
2024-07-28 03:30:00 +0100

    added the notification container

--- src/routes/attendance-register/Attendance-table/tablesContainer.tsx
+++ src/routes/attendance-register/Attendance-table/tablesContainer.tsx
@@ -3,26 +3,25 @@ import { StudentInfoTable } from "./studentInfoTable";
 import { StudentPaymentTable } from "./studentPaymentTable";
 import GroupList from "./groupList";
 import CardlessRegister from "../Attendance-navigation-page/overlays/cardlessRegisterLink";
+import Notifications from "./notifications";
 
 export default function TablesContainer(){
   return(
     
-    <div className="flex w-full gap-[25px] min-h-screen p-4 bg-mainBg">
-
+    <div className="flex items-center w-full gap-[25px] px-4 bg-mainBg">
     <div>
       <StudentInfoTable />
       <StudentPaymentTable />
-      <GroupList/>
+      <GroupList />
       <div className="flex justify-end text-xl py-1">
-        <CardlessRegister/>
-        </div>
+        <CardlessRegister />
+      </div>
     </div>
-    
-    <div className="flex justify-center items-center aligns-center">
-      <CardAnimationSvg />
+    <div >
+      <Notifications />
     </div>
-
   </div>
+  
     
   )
 }
