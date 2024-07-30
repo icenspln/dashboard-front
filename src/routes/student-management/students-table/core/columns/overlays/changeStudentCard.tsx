ba675c59955b1cd173cd4bcf296e90e36a0dfe8b
OmarBknd <omar.bknd@gmail.com>
2024-07-30 13:38:21 +0100

    new file for the change card option

---
+++ src/routes/student-management/students-table/core/columns/overlays/changeStudentCard.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Overlay } from "../../../../../../components/Overlay";
+import SuccessSignSvg from "../../../../../../assets/icons/SuccessSignSvg";
+import ConfirmButton from "./Popup-menu-component/confirmButton";
+
+interface ChangeStudentCardOverlayProps {
+    onClose : () => void
+}
+export default function ChangeStudentCardOverlay({onClose}:ChangeStudentCardOverlayProps){
+    return(
+        <Overlay onClose={onClose}>
+            <>
+            <div className="flex flex-col text-center ">
+                <span >
+                    <h1 className="font-bold">تم تسجيل الطالب بنجاح</h1>
+                    <p className="text-gray-400 mt-3">يمكن اللآن الطالب الدخول للمؤسسة</p>
+                </span>
+                <SuccessSignSvg/>
+                <ConfirmButton text="العودة الى قائمة المسجلين" color="bg-blue" textColor="text-white"/>
+            </div>
+            </>
+        </Overlay>
+    )
+}
