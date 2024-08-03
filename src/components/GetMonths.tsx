db4e17419fa5a347899a2b17a418afdda4025e56
OmarBknd <omar.bknd@gmail.com>
2024-08-03 13:45:58 +0100

    new file that retrieve months and years dynamically

---
+++ src/components/GetMonths.tsx
@@ -0,0 +1,26 @@
+import { format, subYears, addMonths, startOfMonth, startOfYear } from 'date-fns';
+
+const getMonthYearLabel = (date) => format(date, 'MM|yy');
+
+export default function GetMonthYear(startYear) {
+  const options = [];
+  const currentDate = startOfMonth(new Date());
+  const startDate = startOfMonth(new Date(startYear, 0, 1)); 
+
+  let currentMonth = startDate;
+
+ 
+  while (currentMonth <= currentDate) {
+    options.push({ id: options.length + 1, label: getMonthYearLabel(currentMonth) });
+    currentMonth = addMonths(currentMonth, 1);
+  }
+
+  return options;
+}
+
+
+
+const Grrr = GetMonthYear(12)
+console.log(Grrr);
+
+
