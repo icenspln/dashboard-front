85d6731d29c4ccdb01976a7d259f5016064424dc
OmarBknd <omar.bknd@gmail.com>
2024-08-04 05:08:37 +0100

    added a logo

--- src/routes/root/Root.tsx
+++ src/routes/root/Root.tsx
@@ -11,12 +11,18 @@ import ButtonPrimary from "../../components/ButtonPrimary";
 import ButtonSecondary from "../../components/ButtonSecondary";
 import { BLUE, DISABLEDGRAY } from "../../GLOBALS";
 import "./root.css";
+import TempoLogo from "../../assets/icons/TemporaryLogo.png"
 
 export default function Root() {
   return (
     <>
+   
       <div className="">
+      <div className="w-[40px] h-[40px] mt-3 mr-4">
+        <img src={TempoLogo} alt="" />
+        </div>
         <main className="navbar flex">
+          
           <section className="navbar--section fixed w-[240px] top-0 right-0 h-screen   min-h-dvh max-w-md  min-w-md flex flex-col justify-between  p-3 ">
             <ul className="navbar--ul flex flex-col gap-3 mt-[56px] ">
               <li>
