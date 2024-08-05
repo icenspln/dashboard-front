049598315ba9572400fb39e1c2a33069e714987d
OmarBknd <omar.bknd@gmail.com>
2024-08-05 03:52:14 +0100

    create phone number and teacher presence columns

--- src/routes/presence-management/groups-presence/groups-presence-table/core/_models.ts
+++ src/routes/presence-management/groups-presence/groups-presence-table/core/_models.ts
@@ -5,4 +5,5 @@ export type PresenceList = {
   studentName: string;
   dayOfWeek: string;
   pricing:string;
+  phoneNumber:string;
 };
--- src/routes/presence-management/groups-presence/groups-presence-table/core/columns/columns.tsx
+++ src/routes/presence-management/groups-presence/groups-presence-table/core/columns/columns.tsx
@@ -1,6 +1,6 @@
 import { createColumnHelper } from "@tanstack/react-table";
 import { PresenceList } from "../_models";
-import PresentButton from "./isPresentButton";
+import {StudentPresentButton, TeacherPresentButton} from "./isPresentButton";
 import { PricingButton } from "./PricingButtonEdit";
 import { ChangedGroupButton, DeleteFromGroupButton } from "./changeGroupButton";
 
@@ -21,13 +21,21 @@ export const defaultColumns = [
     header: "ثمن الدفع الشهري",
     cell: PricingButton,
   }),
-  columnHelper.accessor("dayOfWeek", {
-    header: "11/08/2024",
-    cell: PresentButton,
+  columnHelper.accessor("phoneNumber", {
+    header: "رقم الهاتف",
+    cell: (info) => info.getValue(),
   }),
   columnHelper.accessor("dayOfWeek", {
     header: "11/08/2024",
-    cell: PresentButton,
+    cell: TeacherPresentButton,
+  }),
+  columnHelper.accessor("dayOfWeek", {
+    header: "11/08/2024",
+    cell: StudentPresentButton,
+  }),
+  columnHelper.accessor("dayOfWeek", {
+    header: "11/08/2024",
+    cell: StudentPresentButton,
   }),
   columnHelper.accessor("dayOfWeek", {
     header: "11/08/2024",
