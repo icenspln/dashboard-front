9648c8d4731e54698be1c113d1963451a9e51aab
icenspln <icenboat@gmail.com>
2024-08-06 00:37:24 +0100

    css root layout fix

--- src/routes/root/Root.tsx
+++ src/routes/root/Root.tsx
@@ -11,19 +11,17 @@ import ButtonPrimary from "../../components/ButtonPrimary";
 import ButtonSecondary from "../../components/ButtonSecondary";
 import { BLUE, DISABLEDGRAY } from "../../GLOBALS";
 import "./root.css";
-import TempoLogo from "../../assets/icons/TemporaryLogo.png"
+import TempoLogo from "../../assets/icons/TemporaryLogo.png";
 
 export default function Root() {
   return (
     <>
-   
       <div className="">
-      <div className="w-[40px] h-[40px] mt-3 mr-4">
-        <img src={TempoLogo} alt="" />
-        </div>
         <main className="navbar flex">
-          
-          <section className="navbar--section fixed w-[240px] top-0 right-0 h-screen   min-h-dvh max-w-md  min-w-md flex flex-col justify-between  p-3 ">
+          <section className="navbar--section fixed w-[240px] top-0 right-0 h-screen   min-h-dvh max-w-md  min-w-md flex flex-col justify-start  p-3 ">
+            <div className="w-[40px] h-[40px] mt-3 mr-4">
+              <img src={TempoLogo} alt="" />
+            </div>
             <ul className="navbar--ul flex flex-col gap-3 mt-[56px] ">
               <li>
                 <NavLink to={`/studentmanagement`}>
@@ -90,10 +88,10 @@ export default function Root() {
                 </NavLink>
               </li>
             </ul>
-            <NavLink to={`/attendancemanagement`}>
-            <ButtonPrimary text="تسجيل الحضور" active>
-              <Signal />
-            </ButtonPrimary>
+            <NavLink className={`mt-auto`} to={`/attendancemanagement`}>
+              <ButtonPrimary text="تسجيل الحضور" active>
+                <Signal />
+              </ButtonPrimary>
             </NavLink>
           </section>
           <section className="mr-[240px] outlet">
