57ab6170e288ba0a3f824a79dd0837850e03bef8
OmarBknd <omar.bknd@gmail.com>
2024-08-07 04:25:45 +0100

    new files for the particular group table

---
+++ src/routes/presence-management/particular-groups-presence/groups-presence-table/core/_models.ts
@@ -0,0 +1,9 @@
+export type PresenceList = {
+  groupId: number;
+  //firstName: string;
+ // lastName: string;
+  studentName: string;
+  dayOfWeek: string;
+  pricing:string;
+  phoneNumber:string;
+};
---
+++ src/routes/presence-management/particular-groups-presence/groups-presence-table/core/columns/columns.tsx
@@ -0,0 +1,49 @@
+import { createColumnHelper } from "@tanstack/react-table";
+import { PresenceList } from "../_models";
+import {StudentPresentButton, TeacherPresentButton} from "../../../../../../components/isPresentButton";
+import { PricingButton } from "../../../../../../components/PricingButtonEdit";
+import { ChangedGroupButton, DeleteFromGroupButton } from "../../../../../../components/changeGroupButton";
+
+const columnHelper = createColumnHelper<PresenceList>();
+
+export const defaultColumns = [
+  columnHelper.accessor("groupId", {
+    header: "الرقم",
+    cell: (info) => info.getValue(),
+  }),
+ 
+  columnHelper.accessor("studentName", {
+    header: "الطالب",
+    cell: (info) => info.getValue(),
+  }),
+ 
+  columnHelper.accessor("pricing", {
+    header: "ثمن الدفع الشهري",
+    cell: PricingButton,
+  }),
+  columnHelper.accessor("phoneNumber", {
+    header: "رقم الهاتف",
+    cell: (info) => info.getValue(),
+  }),
+  columnHelper.accessor("dayOfWeek", {
+    header: "11/08/2024",
+    cell: TeacherPresentButton,
+  }),
+  columnHelper.accessor("dayOfWeek", {
+    header: "11/08/2024",
+    cell: StudentPresentButton,
+  }),
+  columnHelper.accessor("dayOfWeek", {
+    header: "11/08/2024",
+    cell: StudentPresentButton,
+  }),
+  columnHelper.accessor("dayOfWeek", {
+    header: "11/08/2024",
+    cell: ChangedGroupButton,
+  }),
+  columnHelper.accessor("dayOfWeek", {
+    header: "11/08/2024",
+    cell: DeleteFromGroupButton,
+  }),
+
+];
