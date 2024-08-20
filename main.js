96e7d030e1a4f9d5025dbc01d4308237819dadd9
marouanebk <m_boukandoura@estin.dz>
2024-08-20 18:48:10 +0100

    electron

--- main.js
+++ main.js
@@ -1,38 +1,40 @@
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Needed to get __dirname in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 function createWindow() {
-  // Create the browser window.
   const win = new BrowserWindow({
     width: 800,
     height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-    },
+    // webPreferences: {
+    //   preload: path.join(__dirname, 'preload.js'),
+    // },
   });
 
-  //load the index.html from a url
-  win.loadURL("http://localhost:5173");
+  // Load the index.html from the build directory (dist)
+  const startUrl = `file://${path.join(__dirname, './dist/index.html')}`;
+  win.loadURL(startUrl);
 
-  // Open the DevTools.
-  win.webContents.openDevTools();
+  // Open the DevTools in development only
+  // if (process.env.NODE_ENV === 'development') {
+    win.webContents.openDevTools();
+  // }
 }
 
 app.whenReady().then(createWindow);
 
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+app.on('window-all-closed', () => {
+  // if (process.platform !== 'darwin') {
+  //   app.quit();
+  // }
 });
 
-app.on("activate", () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-
+app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist',  // Ensure this matches where Electron is looking for files
+  },
+  base: './',
   plugins: [react()],
 });
