d9345486c098ca9fde82985e3bdf5841f9e9220a
= <=>
2024-08-22 20:46:19 +0100

    pdf fix

--- src/routes/monthly-salary-statement/MonthlySalaryStatement.tsx
+++ src/routes/monthly-salary-statement/MonthlySalaryStatement.tsx
@@ -1,138 +1,139 @@
-import { Link } from "react-router-dom";
-import SalaryStatementTable from "./MonthlySalaryStatementTable";
+import { Link } from "react-router-dom"
+import SalaryStatementTable from "./MonthlySalaryStatementTable"
 import {
-  Document,
-  Page,
-  Text,
-  View,
-  StyleSheet,
-  Font,
-} from "@react-pdf/renderer";
+    Document,
+    Page,
+    Text,
+    View,
+    StyleSheet,
+    Font,
+} from "@react-pdf/renderer"
 
-import { PDFViewer } from "@react-pdf/renderer";
-import { useContext } from "react";
-import { GlobalContext } from "../../GlobalContext";
-import { Teacher } from "../teacher-management/teacher-table/core/_models";
-import { AttendanceForTeacherGroupType } from "../presence-management/teacher-presence/teacher-presence-table/core/_models";
+import { PDFViewer } from "@react-pdf/renderer"
+import { useContext } from "react"
+import { GlobalContext } from "../../GlobalContext"
+import { Teacher } from "../teacher-management/teacher-table/core/_models"
+import { AttendanceForTeacherGroupType } from "../presence-management/teacher-presence/teacher-presence-table/core/_models"
+import amiri from "../../assets/fonts/Amiri-Regular.ttf"
 
 Font.register({
-  family: "Amiri",
-  src: "src/assets/fonts/Amiri-Regular.ttf",
-  fontStyle: "normal",
-  fontWeight: "normal",
-});
+    family: "Amiri",
+    src: amiri,
+    fontStyle: "normal",
+    fontWeight: "normal",
+})
 const styles = StyleSheet.create({
-  page: {
-    flexDirection: "column",
-    fontFamily: "Amiri",
-  },
-  section: {
-    margin: 3,
-    padding: 3,
-    flexGrow: 1,
-    textAlign: "right",
-    alignSelf: "flex-end", // Align each table to the right
-    marginBottom: 10,
-  },
-  header: {
-    margin: 3,
-    height: 40,
-    textAlign: "center",
-  },
-  headerText: {
-    fontSize: 14,
-    fontWeight: "bold",
-  },
-  subHeader: {
-    flexDirection: "row-reverse",
-    justifyContent: "space-around",
-  },
-  subHeaderText: {
-    textAlign: "center",
-  },
-  monthlyPaymentContainer: {
-    display: "flex",
+    page: {
+        flexDirection: "column",
+        fontFamily: "Amiri",
+    },
+    section: {
+        margin: 3,
+        padding: 3,
+        flexGrow: 1,
+        textAlign: "right",
+        alignSelf: "flex-end", // Align each table to the right
+        marginBottom: 10,
+    },
+    header: {
+        margin: 3,
+        height: 40,
+        textAlign: "center",
+    },
+    headerText: {
+        fontSize: 14,
+        fontWeight: "bold",
+    },
+    subHeader: {
+        flexDirection: "row-reverse",
+        justifyContent: "space-around",
+    },
+    subHeaderText: {
+        textAlign: "center",
+    },
+    monthlyPaymentContainer: {
+        display: "flex",
 
-    marginLeft: 10,
-    marginBottom: 10,
-  },
-  monthlyPaymentText: {
-    fontSize: 14,
-    marginLeft: 10,
-    marginBottom: 5,
-  },
-  monthlyPaymentCell: {
-    width: 70,
-    border: "1px solid",
-    borderWidth: 1.5,
-    fontSize: 14,
-    textAlign: "center",
-  },
-});
+        marginLeft: 10,
+        marginBottom: 10,
+    },
+    monthlyPaymentText: {
+        fontSize: 14,
+        marginLeft: 10,
+        marginBottom: 5,
+    },
+    monthlyPaymentCell: {
+        width: 70,
+        border: "1px solid",
+        borderWidth: 1.5,
+        fontSize: 14,
+        textAlign: "center",
+    },
+})
 
 export function MonthlySalaryStatement({
-  teacher,
-  groups,
-  // date,
+    teacher,
+    groups,
+    // date,
 }: {
-  teacher: Teacher;
-  groups: AttendanceForTeacherGroupType[];
-  date: { month: number; year: number };
+    teacher: Teacher
+    groups: AttendanceForTeacherGroupType[]
+    date: { month: number; year: number }
 }) {
-  const totalPayment = groups.reduce((total, group) => {
+    const totalPayment = groups.reduce((total, group) => {
+        return (
+            total +
+            group.students.reduce((groupTotal, student) => {
+                const paidAmount =
+                    student.student.groupFinancials?.groupPaidAmount ?? 0
+                return groupTotal + paidAmount
+            }, 0)
+        )
+    }, 0)
     return (
-      total +
-      group.students.reduce((groupTotal, student) => {
-        const paidAmount =
-          student.student.groupFinancials?.groupPaidAmount ?? 0;
-        return groupTotal + paidAmount;
-      }, 0)
-    );
-  }, 0);
-  return (
-    <Document>
-      <Page size="A4" style={styles.page}>
-        <View>
-          <Text style={styles.header}>كشف الراتب لشهر ديسمبر</Text>
-        </View>
-        <View style={styles.subHeader}>
-          <Text style={styles.subHeaderText}>:الأستاذ</Text>
-          <Text style={styles.subHeaderText}>
-            {teacher.firstName} {teacher.lastName}
-          </Text>
-          <Text style={styles.subHeaderText}>:المادة</Text>
-          <Text style={styles.subHeaderText}>:السنة</Text>
-        </View>
+        <Document>
+            <Page size="A4" style={styles.page}>
+                <View>
+                    <Text style={styles.header}>كشف الراتب لشهر ديسمبر</Text>
+                </View>
+                <View style={styles.subHeader}>
+                    <Text style={styles.subHeaderText}>:الأستاذ</Text>
+                    <Text style={styles.subHeaderText}>
+                        {teacher.firstName} {teacher.lastName}
+                    </Text>
+                    <Text style={styles.subHeaderText}>:المادة</Text>
+                    <Text style={styles.subHeaderText}>:السنة</Text>
+                </View>
 
-        <View style={styles.section}>
-          <SalaryStatementTable groups={groups} />
-        </View>
-        <View style={styles.monthlyPaymentContainer}>
-          <Text style={styles.monthlyPaymentText}>الدفع الشهري</Text>
-          <View style={styles.monthlyPaymentCell}>
-            <Text>{totalPayment}دج</Text>
-          </View>
-        </View>
-      </Page>
-    </Document>
-  );
+                <View style={styles.section}>
+                    <SalaryStatementTable groups={groups} />
+                </View>
+                <View style={styles.monthlyPaymentContainer}>
+                    <Text style={styles.monthlyPaymentText}>الدفع الشهري</Text>
+                    <View style={styles.monthlyPaymentCell}>
+                        <Text>{totalPayment}دج</Text>
+                    </View>
+                </View>
+            </Page>
+        </Document>
+    )
 }
 
 export function PdfViewTest() {
-  const { groups, teacher, date } = useContext(GlobalContext);
+    const { groups, teacher, date } = useContext(GlobalContext)
 
-  console.log("state in the pdf date", date);
-  console.log("state in the pdf", groups, teacher);
-  return (
-    <div style={{ width: "100%", height: "100vh" }}>
-      <PDFViewer style={{ width: "1000px", height: "100%" }}>
-        <MonthlySalaryStatement
-          date={date as { month: number; year: number }}
-          teacher={teacher as Teacher}
-          groups={groups}
-        />
-      </PDFViewer>
-      <Link to="/monthlysalarystatement"></Link>
-    </div>
-  );
+    console.log("state in the pdf date", date)
+    console.log("state in the pdf", groups, teacher)
+    return (
+        <div style={{ width: "100%", height: "100vh" }}>
+            <PDFViewer style={{ width: "1000px", height: "100%" }}>
+                <MonthlySalaryStatement
+                    date={date as { month: number; year: number }}
+                    teacher={teacher as Teacher}
+                    groups={groups}
+                />
+            </PDFViewer>
+            <Link to="/monthlysalarystatement"></Link>
+        </div>
+    )
 }
--- src/routes/monthly-salary-statement/MonthlySalaryStatementTable.tsx
+++ src/routes/monthly-salary-statement/MonthlySalaryStatementTable.tsx
@@ -1,60 +1,58 @@
-
-import { View, StyleSheet, Font } from '@react-pdf/renderer';
-import TableHeader from './core/columns/tableHeader';
-import TableRows from './core/columns/tableRows';
-import { AttendanceForTeacherGroupType } from "../presence-management/teacher-presence/teacher-presence-table/core/_models";
+import { View, StyleSheet, Font } from "@react-pdf/renderer"
+import TableHeader from "./core/columns/tableHeader"
+import TableRows from "./core/columns/tableRows"
+import { AttendanceForTeacherGroupType } from "../presence-management/teacher-presence/teacher-presence-table/core/_models"
+import amiri from "../../assets/fonts/Amiri-Regular.ttf"
 
 Font.register({
-    family: 'Amiri',
-    src: 'src/assets/fonts/Amiri-Regular.ttf',
-    fontStyle: 'normal',
-    fontWeight: 'normal'
-});
+    family: "Amiri",
+    src: amiri,
+    fontStyle: "normal",
+    fontWeight: "normal",
+})
 
 const styles = StyleSheet.create({
     container: {
-        display: 'flex',
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        justifyContent: 'space-between',
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "space-between",
     },
     table: {
         display: "flex",
         // width: "48%",
-        
-      
+
         borderRightWidth: 0,
         borderBottomWidth: 0,
         marginBottom: 10,
     },
-});
+})
 
 function generateTables(groups: AttendanceForTeacherGroupType[]) {
-    const tables: JSX.Element[] = [];
-console.log(groups.toString)
-    
+    const tables: JSX.Element[] = []
+    console.log(groups.toString)
+
     groups.forEach((group, index) => {
         // Adjust rowsPerTable based on your requirements
-        // const rowsPerTable = 24; 
+        // const rowsPerTable = 24;
         console.log("index,", index)
 
         tables.push(
             <View style={styles.table} key={index}>
                 {/* Pass the alldays array to the TableHeader */}
                 <TableHeader alldays={group.alldays} />
-                <TableRows group= {group} /> 
+                <TableRows group={group} />
             </View>
-        );
-    });
+        )
+    })
 
-    return tables;
+    return tables
 }
 
-
-export default function SalaryStatementTable({ groups }: { groups: AttendanceForTeacherGroupType[] }) {
-    return (
-        <View style={styles.container}>
-            {generateTables(groups)}
-        </View>
-    );
+export default function SalaryStatementTable({
+    groups,
+}: {
+    groups: AttendanceForTeacherGroupType[]
+}) {
+    return <View style={styles.container}>{generateTables(groups)}</View>
 }
