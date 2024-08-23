edebbebba5e4482ccbe5e2d2d63ea77966a6692a
OmarBknd <omar.bknd@gmail.com>
2024-08-23 15:06:43 +0100

    new file for the note svg

---
+++ src/assets/icons/NoteSvg.tsx
@@ -0,0 +1,19 @@
+export default function NoteSvg() {
+    return (
+      <svg
+        width="16"
+        height="16"
+        viewBox="0 0 16 16"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM7.26454 1.95996L7.36054 9.30396H8.62454L8.72054 1.95996H7.26454ZM7.24854 11.096V13H8.73654V11.096H7.24854Z"
+          fill="#2B8AF8"
+        />
+      </svg>
+    );
+  }
+  
