c0e2525e0cb5d27a8f5eb06e8c083d72c5d86f11
= <=>
2024-08-27 00:01:08 +0100

    untested fix

--- src/routes/attendance-register/Attendance-table/notifications.tsx
+++ src/routes/attendance-register/Attendance-table/notifications.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from "react";
-import SendMessageSvg from "../../../assets/icons/SendMessageSvg";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { postNoteForStudent } from "./core/_requests";
+import React, { useEffect, useState } from "react"
+import SendMessageSvg from "../../../assets/icons/SendMessageSvg"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { postNoteForStudent } from "./core/_requests"
 // import { useParams } from "react-router-dom";
-import { GetStudentByCardIdType, NoteType } from "./core/_models";
-import toast from "react-hot-toast";
+import { GetStudentByCardIdType, NoteType } from "./core/_models"
+import toast from "react-hot-toast"
 
 // interface Notification {
 //   id: number;
@@ -13,93 +13,100 @@ import toast from "react-hot-toast";
 // }
 
 export default function Notifications({
-  studentInfo,
+    studentInfo,
 }: {
-  studentInfo: GetStudentByCardIdType;
+    studentInfo: GetStudentByCardIdType
 }) {
-  const queryClient = useQueryClient();
-  // const [notifications, setNotifications] = useState<Notification[]>([]);
-  const [inputValue, setInputValue] = useState("");
+    const queryClient = useQueryClient()
+    // const [notifications, setNotifications] = useState<Notification[]>([]);
+    const [inputValue, setInputValue] = useState("")
 
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") {
-      handleSend();
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "Enter") {
+            handleSend()
+        }
     }
-  };
 
-  const [notes, setNotes] = useState<NoteType[]>(studentInfo.notes);
-  // const { id } = useParams();
+    const [notes, setNotes] = useState<NoteType[]>(studentInfo.notes)
 
-  // const { data, isPending, error } = useQuery({
-  //   queryKey: ["getNotesForStudent"],
-  //   queryFn: () => getNotesForStudent(id!),
-  // });
+    useEffect(() => {
+        setNotes(studentInfo.notes)
+    }, [studentInfo])
 
-  const mutation = useMutation({
-    mutationFn: (data: { studentId: string; text: string }) =>
-      postNoteForStudent(data.studentId, data.text),
-    mutationKey: ["postNote"],
-    onSuccess: () => {
-      toast.success("تم الحفض");
-      queryClient.invalidateQueries({ queryKey: ["getNotesForStudent"] });
-    },
-    onError: () => {
-      toast.error("حدث خطأ ما");
-      queryClient.invalidateQueries({ queryKey: ["getNotesForStudent"] });
-    },
-  });
+    // const { id } = useParams();
 
-  const handleSend = () => {
-    if (inputValue.trim() === "") return;
-    // const newNotification: Notification = {
-    //   id: Date.now(),
-    //   message: inputValue,
-    //   timestamp: new Date().toLocaleString(),
-    // };
+    // const { data, isPending, error } = useQuery({
+    //   queryKey: ["getNotesForStudent"],
+    //   queryFn: () => getNotesForStudent(id!),
+    // });
 
-    // setNotifications([...notifications, newNotification]);
-    const data = {
-      studentId: studentInfo.student._id!,
-      text: inputValue,
-    };
-    mutation.mutate(data);
-    setInputValue("");
-  };
+    const mutation = useMutation({
+        mutationFn: (data: { studentId: string; text: string }) =>
+            postNoteForStudent(data.studentId, data.text),
+        mutationKey: ["postNote"],
+        onSuccess: () => {
+            toast.success("تم الحفض")
+            queryClient.invalidateQueries({ queryKey: ["getNotesForStudent"] })
+        },
+        onError: () => {
+            toast.error("حدث خطأ ما")
+            queryClient.invalidateQueries({ queryKey: ["getNotesForStudent"] })
+        },
+    })
 
-  useEffect(() => {
-    if (mutation.data && !mutation.error) {
-      setNotes((prev) => [...prev, mutation.data]);
+    const handleSend = () => {
+        if (inputValue.trim() === "") return
+        // const newNotification: Notification = {
+        //   id: Date.now(),
+        //   message: inputValue,
+        //   timestamp: new Date().toLocaleString(),
+        // };
+
+        // setNotifications([...notifications, newNotification]);
+        const data = {
+            studentId: studentInfo.student._id!,
+            text: inputValue,
+        }
+        mutation.mutate(data)
+        setInputValue("")
     }
-  }, [mutation.data, mutation.isError]);
 
-  if (notes)
-    return (
-      <div className="h-[700px] overflow-auto border border-gray-200 rounded-lg shadow-sm p-4 flex flex-col justify-between">
-        <div className="flex flex-col gap-2 overflow-y-auto h-auto max-h-[807px]">
-          {notes.map((notification) => (
-            <div key={notification._id}>
-              <div className="text-sm text-center text-gray-300">
-                {new Date(notification.date).toLocaleDateString()}
-              </div>
-              <div className="bg-blue text-white p-2 rounded-lg">
-                <div>{notification.text}</div>
-              </div>
+    useEffect(() => {
+        if (mutation.data && !mutation.error) {
+            setNotes((prev) => [...prev, mutation.data])
+        }
+    }, [mutation.data, mutation.isError])
+
+    if (notes)
+        return (
+            <div className="h-[700px] overflow-auto border border-gray-200 rounded-lg shadow-sm p-4 flex flex-col justify-between">
+                <div className="flex flex-col gap-2 overflow-y-auto h-auto max-h-[807px]">
+                    {notes.map((notification) => (
+                        <div key={notification._id}>
+                            <div className="text-sm text-center text-gray-300">
+                                {new Date(
+                                    notification.date
+                                ).toLocaleDateString()}
+                            </div>
+                            <div className="bg-blue text-white p-2 rounded-lg">
+                                <div>{notification.text}</div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                <div className="flex items-center gap-2 mt-2">
+                    <input
+                        type="text"
+                        placeholder="اكتب ملاحضتك هنا"
+                        value={inputValue}
+                        onChange={(e) => setInputValue(e.target.value)}
+                        onKeyDown={handleKeyDown}
+                        className="outline-none rounded-full w-full h-[41px] p-2 border border-gray-300"
+                    />
+                    <button onClick={handleSend}>
+                        <SendMessageSvg />
+                    </button>
+                </div>
             </div>
-          ))}
-        </div>
-        <div className="flex items-center gap-2 mt-2">
-          <input
-            type="text"
-            placeholder="اكتب ملاحضتك هنا"
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
-            onKeyDown={handleKeyDown}
-            className="outline-none rounded-full w-full h-[41px] p-2 border border-gray-300"
-          />
-          <button onClick={handleSend}>
-            <SendMessageSvg />
-          </button>
-        </div>
-      </div>
-    );
+        )
 }
