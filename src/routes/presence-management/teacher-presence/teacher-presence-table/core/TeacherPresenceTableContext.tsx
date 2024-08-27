f9ac40e999aab8e715a7ad6a5959c133eee18b20
= <=>
2024-08-27 20:37:11 +0100

    state problem

--- src/routes/presence-management/teacher-presence/teacher-presence-table/core/TeacherPresenceTableContext.tsx
+++ src/routes/presence-management/teacher-presence/teacher-presence-table/core/TeacherPresenceTableContext.tsx
@@ -1,89 +1,100 @@
-import queryString from "query-string";
 import {
-  Dispatch,
-  ReactNode,
-  SetStateAction,
-  useContext,
-  useEffect,
-  useState,
-} from "react";
-import { createContext } from "react";
+    Dispatch,
+    ReactNode,
+    SetStateAction,
+    useContext,
+    useEffect,
+    useState,
+} from "react"
+import { createContext } from "react"
+import { useLocation } from "react-router-dom"
 
 type teacherPresenceTableContext = {
-  filterState: { month: string[]; year: string[] };
-  setFilterState: Dispatch<SetStateAction<{ month: string[]; year: string[] }>>;
-  filter: string;
-  setFilter: Dispatch<SetStateAction<string>>;
-};
+    filterState: { month: string[]; year: string[] }
+    setFilterState: Dispatch<
+        SetStateAction<{ month: string[]; year: string[] }>
+    >
+    filter: string
+    setFilter: Dispatch<SetStateAction<string>>
+}
 
 export const teacherpresenceTableContext =
-  createContext<teacherPresenceTableContext>({
-    filterState: {
-      year: [""],
-      month: [""],
-    },
-    setFilterState: () => {},
-    filter: "",
-    setFilter: () => {},
-  });
+    createContext<teacherPresenceTableContext>({
+        filterState: {
+            year: [""],
+            month: [""],
+        },
+        setFilterState: () => {},
+        filter: "",
+        setFilter: () => {},
+    })
 
 export function TeacherPresenceTableContextProvider({
-  children,
+    children,
 }: {
-  children: ReactNode;
+    children: ReactNode
 }) {
-  const [filter, setFilter] = useState<string>("");
-  console.log("filter being set", filter);
+    const [filter, setFilter] = useState<string>("")
 
-  const { month, year } = queryString.parse(location.search) as any;
-  const [filterState, setFilterState] = useState<{
-    year: string[];
-    month: string[];
-  }>({
-    month: month,
-    year: year,
-  });
+    function useQuery() {
+        const { hash } = useLocation()
+        const queryString = hash.split("?")[1] || ""
+        return new URLSearchParams(queryString)
+    }
 
-  useEffect(() => {
-    if (month && year) {
-      setFilterState({
+    const query = useQuery()
+    const month = query.get("month") as any
+    const year = query.get("year") as any
+
+    // const { month, year } = queryString.parse(location.search) as any
+    const [filterState, setFilterState] = useState<{
+        year: string[]
+        month: string[]
+    }>({
         month: month,
         year: year,
-      });
-    }
-  }, []);
-  useEffect(() => {
-    const month = `month=${filterState.month}`;
-    const year = `year=${filterState.year}`;
-    let url = "";
-    if (filterState.month) {
-      if (url) url += "&" + month;
-      if (!url) url += month;
-    }
-    if (filterState.year) {
-      if (url) url += "&" + year;
-      if (!url) url += year;
-    }
+    })
 
-    setFilter(url);
-  }, [filterState]);
+    useEffect(() => {
+        if (month && year) {
+            setFilterState({
+                month: month,
+                year: year,
+            })
+        }
+    }, [])
+    useEffect(() => {
+        const month = `month=${filterState.month}`
+        const year = `year=${filterState.year}`
+        let url = ""
+        if (filterState.month) {
+            if (url) url += "&" + month
+            if (!url) url += month
+        }
+        if (filterState.year) {
+            if (url) url += "&" + year
+            if (!url) url += year
+        }
 
-  return (
-    <teacherpresenceTableContext.Provider
-      value={{
-        filterState,
-        setFilterState,
-        setFilter,
-        filter,
-      }}
-    >
-      {children}
-    </teacherpresenceTableContext.Provider>
-  );
+        setFilter(url)
+    }, [filterState])
+
+    return (
+        <teacherpresenceTableContext.Provider
+            value={{
+                filterState,
+                setFilterState,
+                setFilter,
+                filter,
+            }}
+        >
+            {children}
+        </teacherpresenceTableContext.Provider>
+    )
 }
 
 export const useTeacherPresenceContext = () => {
-  const context = useContext(teacherpresenceTableContext);
-  if (context) return context;
-  return {};
-};
+    const context = useContext(teacherpresenceTableContext)
+    if (context) return context
+    return {}
+}
--- src/routes/teacher-management/teacher-table/core/columns/overlays/downloadReceipt.tsx
+++ src/routes/teacher-management/teacher-table/core/columns/overlays/downloadReceipt.tsx
@@ -1,122 +1,130 @@
-import React, { useState } from "react";
-import { Overlay } from "../../../../../../components/Overlay";
-import ConfirmButton from "../../../../../../components/confirmButton";
+import React, { useState } from "react"
+import { Overlay } from "../../../../../../components/Overlay"
+import ConfirmButton from "../../../../../../components/confirmButton"
 // import PasswordInput from "./Popup-menu-component/passwordInputField";
-import Checklist from "../../../../../../components/CheckList";
-import { useNavigate } from "react-router-dom";
-import GetMonthYear from "../../../../../../components/GetMonths";
-import { useSettings,  } from "../../../../../settings/core/SettingsContext";
-import PasswordInput from "../../../../../settings/passwordInputField";
+import Checklist from "../../../../../../components/CheckList"
+import { useNavigate } from "react-router-dom"
+import GetMonthYear from "../../../../../../components/GetMonths"
+import { useSettings } from "../../../../../settings/core/SettingsContext"
+import PasswordInput from "../../../../../settings/passwordInputField"
 
-const chooseMonthOption = GetMonthYear(2024);
+const chooseMonthOption = GetMonthYear(2024)
 interface DownloadReceiptOverlayProps {
-  onClose: () => void;
-  teacherId: string;
+    onClose: () => void
+    teacherId: string
 }
 
 const DownloadReceiptOverlay: React.FC<DownloadReceiptOverlayProps> = ({
-  onClose,
-  teacherId,
+    onClose,
+    teacherId,
 }) => {
-  //state for filtering in teacher pdf
+    //state for filtering in teacher pdf
 
-  const {
- 
-    checkPassword,
-  } = useSettings();
-  const [timeQuery, setTimeQuery] = useState<{ month: number; year: number }>();
-  const [currentView, setCurrentView] = useState<"checklist" | "password">(
-    "checklist"
-  );
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-  const navigate = useNavigate();  // const handleConfirmClick = () => {
-  //   if (currentView === "checklist") {
-  //     setCurrentView("password");
-  //   } else if (currentView === "password") {
-  //     if (timeQuery) {
-  //       navigate(
-  //         `/teachermanagement/teacherpayment/${teacherId}?month=${timeQuery?.month}&year=${timeQuery?.year}`
-  //       );
-  //       return;
-  //     }
-  //     navigate(`/teachermanagement/teacherpayment/${teacherId}`);
-  //   }
-  // };
-  const handleConfirmClick = async () => {
-    if (currentView === "checklist") {
-      setCurrentView("password");
-    } else if (currentView === "password") {
-      try {
-        const isValid = await checkPassword(password);
-        if (isValid) {
-          setError("");
-          if (timeQuery) {
-            navigate(
-              `/teachermanagement/teacherpayment/${teacherId}?month=${timeQuery?.month}&year=${timeQuery?.year}`
-            );
-          } else {
-            navigate(`/teachermanagement/teacherpayment/${teacherId}`);
-          }
-        } else {
-          setError("كلمة المرور غير صحيحة");
+    const { checkPassword } = useSettings()
+    const [timeQuery, setTimeQuery] = useState<{
+        month: number
+        year: number
+    }>()
+    const [currentView, setCurrentView] = useState<"checklist" | "password">(
+        "checklist"
+    )
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState("")
+    const navigate = useNavigate() // const handleConfirmClick = () => {
+    //   if (currentView === "checklist") {
+    //     setCurrentView("password");
+    //   } else if (currentView === "password") {
+    //     if (timeQuery) {
+    //       navigate(
+    //         `/teachermanagement/teacherpayment/${teacherId}?month=${timeQuery?.month}&year=${timeQuery?.year}`
+    //       );
+    //       return;
+    //     }
+    //     navigate(`/teachermanagement/teacherpayment/${teacherId}`);
+    //   }
+    // };
+    const handleConfirmClick = async () => {
+        if (currentView === "checklist") {
+            setCurrentView("password")
+        } else if (currentView === "password") {
+            try {
+                const isValid = await checkPassword(password)
+                if (isValid) {
+                    setError("")
+                    if (timeQuery) {
+                        navigate(
+                            `/teachermanagement/teacherpayment/${teacherId}/#?month=${timeQuery?.month}&year=${timeQuery?.year}`
+                        )
+                    } else {
+                        navigate(
+                            `/teachermanagement/teacherpayment/${teacherId}`
+                        )
+                    }
+                } else {
+                    setError("كلمة المرور غير صحيحة")
+                }
+            } catch {
+                setError("حدث خطأ ما. حاول مرة أخرى.")
+            }
         }
-      } catch {
-        setError("حدث خطأ ما. حاول مرة أخرى.");
-      }
     }
-  };
 
-  return (
-    <Overlay onClose={onClose}>
-      <>
-        <div className="w-[511px] flex flex-col items-center gap-[15px]">
-          {currentView === "checklist" ? (
+    return (
+        <Overlay onClose={onClose}>
             <>
-              <h1 className="text-2xl">تحميل كشف الراتب الشهري</h1>
-              <p>
-                يرجى اختيار شهر الدفع من أجل تحميل كشف الراتب الشهري لهذا
-                الأستاذ
-              </p>
-              <div className="flex w-full justify-between items-center gap-[12px]">
-                <div className="flex-1">
-                  <Checklist
-                    items={chooseMonthOption}
-                    setState={setTimeQuery}
-                  />
+                <div className="w-[511px] flex flex-col items-center gap-[15px]">
+                    {currentView === "checklist" ? (
+                        <>
+                            <h1 className="text-2xl">
+                                تحميل كشف الراتب الشهري
+                            </h1>
+                            <p>
+                                يرجى اختيار شهر الدفع من أجل تحميل كشف الراتب
+                                الشهري لهذا الأستاذ
+                            </p>
+                            <div className="flex w-full justify-between items-center gap-[12px]">
+                                <div className="flex-1">
+                                    <Checklist
+                                        items={chooseMonthOption}
+                                        setState={setTimeQuery}
+                                    />
+                                </div>
+                                <ConfirmButton
+                                    text="التالي"
+                                    className="text-white w-[91px]"
+                                    onClick={handleConfirmClick}
+                                />
+                            </div>
+                        </>
+                    ) : (
+                        <>
+                            <h1 className="text-2xl">أدخل كلمة المرور</h1>
+                            <p>يرجى إدخال كلمة المرور قبل تحميل قسيمة الدفع</p>
+                            <div className="flex w-full justify-between items-center gap-[12px]">
+                                <div className="flex-1">
+                                    <PasswordInput
+                                        placeHolder="أدخل كلمة المرور"
+                                        value={password}
+                                        onChange={(e) =>
+                                            setPassword(e.target.value)
+                                        }
+                                    />
+                                </div>
+                                <ConfirmButton
+                                    text="التالي"
+                                    className="text-white w-[90px]"
+                                    onClick={handleConfirmClick}
+                                />
+                            </div>
+                            {error && (
+                                <p className="text-red-500 mt-2">{error}</p>
+                            )}
+                        </>
+                    )}
                 </div>
-                <ConfirmButton
-                  text="التالي"
-                  className="text-white w-[91px]"
-                  onClick={handleConfirmClick}
-                />
-              </div>
             </>
-          ) : (
-            <>
-            <h1 className="text-2xl">أدخل كلمة المرور</h1>
-            <p>يرجى إدخال كلمة المرور قبل تحميل قسيمة الدفع</p>
-            <div className="flex w-full justify-between items-center gap-[12px]">
-              <div className="flex-1">
-                <PasswordInput
-                  placeHolder="أدخل كلمة المرور"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-              <ConfirmButton
-                text="التالي"
-                className="text-white w-[90px]"
-                onClick={handleConfirmClick}
-              />
-            </div>
-            {error && <p className="text-red-500 mt-2">{error}</p>}
-          </>
-          )}
-        </div>
-      </>
-    </Overlay>
-  );
-};
+        </Overlay>
+    )
+}
 
-export default DownloadReceiptOverlay;
+export default DownloadReceiptOverlay
