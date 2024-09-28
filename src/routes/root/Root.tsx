95a8a721ef1c3b7d9cd479baa0198ef2adb2d183
icenspln <icenboat@gmail.com>
2024-09-28 15:19:14 +0100

    sidebar into english

--- src/routes/root/Root.tsx
+++ src/routes/root/Root.tsx
@@ -1,122 +1,160 @@
-import { NavLink, Outlet } from "react-router-dom";
-import {
-  Book,
-  Bookmark,
-  Gear,
-  Prof,
-  Signal,
-  Student,
-} from "../../assets/icons";
-import BriefCase from "../../assets/icons/BriefCaseSvg";
-import ButtonPrimary from "../../components/ButtonPrimary";
-import ButtonSecondary from "../../components/ButtonSecondary";
-import { BLUE, DISABLEDGRAY } from "../../GLOBALS";
-import "./root.css";
-import { useSettings } from "../settings/core/SettingsContext";
+import { NavLink, Outlet } from "react-router-dom"
+import { Book, Bookmark, Gear, Prof, Signal, Student } from "../../assets/icons"
+import BriefCase from "../../assets/icons/BriefCaseSvg"
+import ButtonPrimary from "../../components/ButtonPrimary"
+import ButtonSecondary from "../../components/ButtonSecondary"
+import { BLUE, DISABLEDGRAY } from "../../GLOBALS"
+import "./root.css"
+import { useSettings } from "../settings/core/SettingsContext"
 
 export default function Root() {
-  const { logoUrl } = useSettings();
+    const { logoUrl } = useSettings()
 
-  return (
-    <>
-      <main className="navbar flex">
-        <div className="fixed">
-          <div className="w-[42px] h-[42px] mt-3 mr-5">
-            <img src={logoUrl} alt="" />
-          </div>
-          <section className="navbar--section fixed w-[240px] top-0 right-0    min-h-dvh max-w-md  min-w-md flex flex-col justify-between  p-3 ">
-            <ul className="navbar--ul flex flex-col gap-3 mt-[56px] ">
-              <li>
-                <NavLink to={`/studentmanagement`}>
-                  {({ isActive }) => (
-                    <>
-                      <ButtonSecondary
-                        text="تسيير المسجلين"
-                        isActive={isActive}
-                      >
-                        <Student fill={isActive ? BLUE : DISABLEDGRAY} />
-                      </ButtonSecondary>
-                    </>
-                  )}
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to={`/teachermanagement`}>
-                  {({ isActive }) => (
-                    <>
-                      <ButtonSecondary
-                        text="تسيير الأساتذة"
-                        isActive={isActive}
-                      >
-                        <Prof fill={isActive ? BLUE : DISABLEDGRAY} />
-                      </ButtonSecondary>
-                    </>
-                  )}
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to={`/groupmanagement`}>
-                  {({ isActive }) => (
-                    <>
-                      <ButtonSecondary text="تسيير الأفواج" isActive={isActive}>
-                        <Book fill={isActive ? BLUE : DISABLEDGRAY} />
-                      </ButtonSecondary>
-                    </>
-                  )}
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to={`/particulargroupmanagement`}>
-                  {({ isActive }) => (
-                    <>
-                      <ButtonSecondary
-                        text="تسيير الأفواج الخاصة"
-                        isActive={isActive}
-                      >
-                        <Bookmark fill={isActive ? BLUE : DISABLEDGRAY} />
-                      </ButtonSecondary>
-                    </>
-                  )}
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to={`/employeemanagement`}>
-                  {({ isActive }) => (
-                    <>
-                      <ButtonSecondary
-                        text="تسيير حضور العمال"
-                        isActive={isActive}
-                      >
-                        <BriefCase fill={isActive ? BLUE : DISABLEDGRAY} />
-                      </ButtonSecondary>
-                    </>
-                  )}
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to={`/settings`}>
-                  {({ isActive }) => (
-                    <>
-                      <ButtonSecondary text="الإعدادات" isActive={isActive}>
-                        <Gear fill={isActive ? BLUE : DISABLEDGRAY} />
-                      </ButtonSecondary>
-                    </>
-                  )}
-                </NavLink>
-              </li>
-            </ul>
+    return (
+        <>
+            <main className="navbar flex">
+                <div className="fixed">
+                    <div className="w-[42px] h-[42px] mt-3 mr-5">
+                        <img src={logoUrl} alt="" />
+                    </div>
+                    <section className="navbar--section fixed w-[240px] top-0 left-0    min-h-dvh max-w-md  min-w-md flex flex-col justify-between  p-3 ">
+                        <ul className="navbar--ul flex flex-col gap-3 mt-[56px] ">
+                            <li>
+                                <NavLink to={`/studentmanagement`}>
+                                    {({ isActive }) => (
+                                        <>
+                                            <ButtonSecondary
+                                                text="Students"
+                                                isActive={isActive}
+                                            >
+                                                <Student
+                                                    fill={
+                                                        isActive
+                                                            ? BLUE
+                                                            : DISABLEDGRAY
+                                                    }
+                                                />
+                                            </ButtonSecondary>
+                                        </>
+                                    )}
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to={`/teachermanagement`}>
+                                    {({ isActive }) => (
+                                        <>
+                                            <ButtonSecondary
+                                                text="Teachers"
+                                                isActive={isActive}
+                                            >
+                                                <Prof
+                                                    fill={
+                                                        isActive
+                                                            ? BLUE
+                                                            : DISABLEDGRAY
+                                                    }
+                                                />
+                                            </ButtonSecondary>
+                                        </>
+                                    )}
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to={`/groupmanagement`}>
+                                    {({ isActive }) => (
+                                        <>
+                                            <ButtonSecondary
+                                                text="Groups"
+                                                isActive={isActive}
+                                            >
+                                                <Book
+                                                    fill={
+                                                        isActive
+                                                            ? BLUE
+                                                            : DISABLEDGRAY
+                                                    }
+                                                />
+                                            </ButtonSecondary>
+                                        </>
+                                    )}
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to={`/particulargroupmanagement`}>
+                                    {({ isActive }) => (
+                                        <>
+                                            <ButtonSecondary
+                                                text="Private Groups"
+                                                isActive={isActive}
+                                            >
+                                                <Bookmark
+                                                    fill={
+                                                        isActive
+                                                            ? BLUE
+                                                            : DISABLEDGRAY
+                                                    }
+                                                />
+                                            </ButtonSecondary>
+                                        </>
+                                    )}
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to={`/employeemanagement`}>
+                                    {({ isActive }) => (
+                                        <>
+                                            <ButtonSecondary
+                                                text="Employees"
+                                                isActive={isActive}
+                                            >
+                                                <BriefCase
+                                                    fill={
+                                                        isActive
+                                                            ? BLUE
+                                                            : DISABLEDGRAY
+                                                    }
+                                                />
+                                            </ButtonSecondary>
+                                        </>
+                                    )}
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to={`/settings`}>
+                                    {({ isActive }) => (
+                                        <>
+                                            <ButtonSecondary
+                                                text="Settings"
+                                                isActive={isActive}
+                                            >
+                                                <Gear
+                                                    fill={
+                                                        isActive
+                                                            ? BLUE
+                                                            : DISABLEDGRAY
+                                                    }
+                                                />
+                                            </ButtonSecondary>
+                                        </>
+                                    )}
+                                </NavLink>
+                            </li>
+                        </ul>
 
-            <NavLink className={`mt-auto`} to={`/attendancemanagement`}>
-              <ButtonPrimary text="تسجيل الحضور" active>
-                <Signal />
-              </ButtonPrimary>
-            </NavLink>
-          </section>
-        </div>
-        <section className="mr-[240px] outlet">
-          <Outlet />
-        </section>
-      </main>
-    </>
-  );
+                        <NavLink
+                            className={`mt-auto`}
+                            to={`/attendancemanagement`}
+                        >
+                            <ButtonPrimary text="Enroll Student" active>
+                                <Signal />
+                            </ButtonPrimary>
+                        </NavLink>
+                    </section>
+                </div>
+                <section className="ml-[240px] outlet">
+                    <Outlet />
+                </section>
+            </main>
+        </>
+    )
 }
