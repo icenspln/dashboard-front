d0af01ca06b66143f6caeb9fa3e3a5211759eb91
icenspln <icenboat@gmail.com>
2024-10-02 18:35:08 +0100

    https

--- src/api/Axios.ts
+++ src/api/Axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const AxiosInstance = axios.create({
-    baseURL: "http://siradj-backend-production.up.railway.app:8080/api",
+    baseURL: "https://siradj-backend-production.up.railway.app:8080/api",
     timeout: 1000,
     //   headers: {'X-Custom-Header': 'foobar'}
 });
