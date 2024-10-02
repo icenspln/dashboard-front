46085d98eb2ad57087c6bb71bd4c169aaad3c757
icenspln <icenboat@gmail.com>
2024-10-02 19:24:17 +0100

    port fix

--- src/api/Axios.ts
+++ src/api/Axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const AxiosInstance = axios.create({
-    baseURL: "https://siradj-backend-production.up.railway.app:8080/api",
+   baseURL: "https://siradj-backend-production.up.railway.app:443/api",
+  //  baseURL: "http://localhost:8080/api",
     timeout: 1000,
     //   headers: {'X-Custom-Header': 'foobar'}
 });
