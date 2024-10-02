f32d7e7a1551b6f75f2cd46f05fe350e9a9440bd
icenspln <icenboat@gmail.com>
2024-10-02 18:28:17 +0100

    app name change

--- src/api/Axios.ts
+++ src/api/Axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const AxiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:4000/api",
-  timeout: 1000,
-  //   headers: {'X-Custom-Header': 'foobar'}
+    baseURL: "http://siradj-backend-production.up.railway.app:8080/api",
+    timeout: 1000,
+    //   headers: {'X-Custom-Header': 'foobar'}
 });
