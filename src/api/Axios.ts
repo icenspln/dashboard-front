fc7dc9d51522e987f925ce22bd645ca458fe80bb
icenspln <icenboat@gmail.com>
2024-10-03 15:23:50 +0100

    headers

--- src/api/Axios.ts
+++ src/api/Axios.ts
@@ -4,4 +4,8 @@ export const AxiosInstance = axios.create({
     baseURL: "https://siradj-backend-production.up.railway.app:443/api",
     //  baseURL: "http://localhost:8080/api",
     timeout: 1000,
+    headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "no-cache",
+    },
 });
